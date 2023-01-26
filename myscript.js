e94a166cea923eb7445dc33037825d3eebc255d4
Daniel Burgess <dan-burgess@hotmail.com>
2023-01-26 11:37:27 +1030

    refactored

--- myscript.js
+++ myscript.js
@@ -1,9 +1,14 @@
 
-// Function to get computers choice
+// let getComputerChoice = function() {
+  //   const options = ["paper", "scissors", "rock"]
+  //   return options[Math.floor(Math.random() * options.length)];
+  // }
+
+  // Function to get computers choice
+
 function getComputerChoice() {
   const options = ["paper", "scissors", "rock"]
-  const selection = options[Math.floor(Math.random()*options.length)];
-  return selection
+  return options[Math.floor(Math.random() * options.length)];
 }
-
 console.log(getComputerChoice());
+
