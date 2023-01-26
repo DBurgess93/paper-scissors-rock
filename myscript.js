31b9e1343add4a441bf146b21240a995c73284b5
Daniel Burgess <dan-burgess@hotmail.com>
2023-01-26 12:54:40 +1030

    Create gamePlay function - plays one round

--- myscript.js
+++ myscript.js
@@ -10,11 +10,29 @@ function getComputerChoice() {
   const options = ["paper", "scissors", "rock"]
   return options[Math.floor(Math.random() * options.length)];
 }
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
+// Function to get players choice
 function playerSelection() {
   const selection = prompt("What is your selection? ");
   return selection.toLowerCase();
 }
+// console.log(playerSelection());
 
-console.log(playerSelection());
+function gamePlay(computer, player) {
+  computer = getComputerChoice()
+  player = playerSelection()
+  console.log(computer)
+  console.log(player)
+  if (computer === player) {
+    return "Draw! Go again"
+  } else if (computer === "paper" && player === "rock") {
+    return "Computer wins.."
+  } else if (computer === "scissors" && player === "paper") {
+    return "Computer wins.."
+  } else if (computer === "rock" && player === "scissors") {
+    return "Computer wins.."
+  }
+  else return "You win!"
+}
+console.log(gamePlay());
