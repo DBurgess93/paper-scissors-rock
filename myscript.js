6220e2e05dddfb8f49c84f2fd7f600b279629274
Daniel Burgess <dan-burgess@hotmail.com>
2023-01-31 23:01:34 +1100

    erase comments

--- myscript.js
+++ myscript.js
@@ -1,16 +1,9 @@
 
-// let getComputerChoice = function() {
-  //   const options = ["paper", "scissors", "rock"]
-  //   return options[Math.floor(Math.random() * options.length)];
-  // }
-
-  // Function to get computers choice
 
 function getComputerChoice() {
   const options = ["paper", "scissors", "rock"]
   return options[Math.floor(Math.random() * options.length)];
 }
-// console.log(getComputerChoice());
 
 // Function to get players choice
 function playerSelection() {
